2ec3134eb2a82d046c0a6a5ca5439e2f8344ebc4
Vadim Abzalov <v.abzalov@gmail.com>
2020-11-20 09:35:09 +0500

    Add linked list implementation. Change slicer according OOP style

--- task2/pkg/slicer/slicer.go
+++ task2/pkg/slicer/slicer.go
@@ -1,6 +1,14 @@
 package slicer
 
-func Insert(x int, sortedSlice []int) []int {
+import (
+	"errors"
+	"fmt"
+)
+
+type SliceManager struct {
+}
+
+func (slicer *SliceManager) Insert(x int, sortedSlice []int) []int {
 	var tmp = make([]int, 0, len(sortedSlice)+1)
 
 	if len(sortedSlice) == 0 || x < sortedSlice[0] {
@@ -27,24 +35,7 @@ func Insert(x int, sortedSlice []int) []int {
 	return tmp
 }
 
-func Insert2(x int, sortedSlice []int) []int {
-	tmp := make([]int, len(sortedSlice)+1)
-	copy(tmp, sortedSlice)
-
-	for i, v := range sortedSlice {
-		if x < v {
-			copy(tmp[i+1:], tmp[i:])
-			tmp[i] = x
-
-			return tmp
-		}
-	}
-
-	tmp[len(sortedSlice)] = x
-	return tmp
-}
-
-func Delete(x int, anySlice []int) []int {
+func (slicer *SliceManager) Delete(x int, anySlice []int) []int {
 	var tmp = make([]int, 0, len(anySlice))
 	for _, elem := range anySlice {
 		if x == elem {
@@ -54,4 +45,130 @@ func Delete(x int, anySlice []int) []int {
 	}
 
 	return tmp
+}
+
+// Linked list implementation
+
+type ListItem struct {
+	value      int
+	prev, next *ListItem
+}
+
+type List struct {
+	len  int
+	head *ListItem
+}
+
+func (L *List) DisplayChain() string {
+	var chain string
+	item := L.head
+	for item != nil {
+		if item.next != nil {
+			chain = chain + fmt.Sprintf("%d -> ", item.value)
+		} else {
+			chain = chain + fmt.Sprintf("%d", item.value)
+		}
+		item = item.next
+	}
+
+	return chain
+}
+
+func (L *List) Insert(value int) {
+	newItem := &ListItem{
+		value: value,
+	}
+
+	item := L.head
+	for item != nil {
+		if value <= item.value {
+
+			if item == L.head {
+				L.head = newItem
+			}
+
+			if item.prev != nil {
+				item.prev.next = newItem
+				newItem.prev = item.prev
+			}
+
+			item.prev = newItem
+			newItem.next = item
+			break
+		}
+
+		if item.next == nil {
+			item.next = newItem
+			newItem.prev = item
+			break
+		}
+
+		item = item.next
+	}
+
+	if L.head == nil {
+		L.head = newItem
+	}
+
+	L.len++
+}
+
+func (L *List) Delete(value int) {
+	item := L.head
+
+	if item == nil {
+		return
+	}
+
+	for item != nil {
+		if item.value == value {
+
+			if item.prev != nil {
+				item.prev.next = item.next
+			}
+
+			if item.next != nil {
+				item.next.prev = item.prev
+			}
+
+			if item == L.head {
+				L.head = item.next
+			}
+
+			L.len--
+		}
+		item = item.next
+	}
+}
+
+func (L *List) getMax() (max int, err error) {
+	item := L.head
+	if item == nil {
+		return 0, errors.New("the list is empty")
+	}
+
+	max = item.value
+	for item != nil {
+		if item.value > max {
+			max = item.value
+		}
+		item = item.next
+	}
+	return max, nil
+}
+
+func (L *List) getMin() (min int, err error) {
+	item := L.head
+	if item == nil {
+		return 0, errors.New("the list is empty")
+	}
+
+	min = item.value
+	for item != nil {
+		if item.value < min {
+			min = item.value
+		}
+		item = item.next
+	}
+	return min, nil
 }
